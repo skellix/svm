4779b32e1d067b80e04b735b83f65b13f86c8c26
skellix <24k911@gmail.com>
2015-03-15 22:38:39 +0000

    Added compatibility for local variables and two byte operations

--- svm.c
+++ svm.c
@@ -1,4 +1,4 @@
-#!/usr/local/bin/tcc -run -g -L/usr/lib -lgc
+#!/usr/local/bin/tcc -run -g -I~/workspace/test10 -L/usr/lib -lgc
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -12,43 +12,71 @@ void showHelp(){
 }
 
 int debug = 0;
+int error = 0;
+char* errorMessage = NULL;
 
-int runBytecode(Bytecode* main, Stack* stack, long* lineNumber);
+int runBytecode(Bytecode* main, Stack* stack, Stack* localIndex, Stack* locals, Stack* localFrame, long* lineNumber);
 void dumpStack(Stack* stack, FILE* stream);
+void dumpLocals(Stack* localIndex, Stack* locals, Stack* localFrame, FILE* stream);
+void setError(char* message);
 int main(int argc, char** argv) {
 	if (argc <= 1) {
 		showHelp(); return 0;
 	}
+	int norun = 0;
 	for (int i = 1 ; i < argc ; i ++) {
+		if (strcmp(argv[i], "-E") == 0) {
+			norun = 1;
+		}
 		if (strcmp(argv[i], "-X") == 0) {
 			debug ++;
 		}
 	}
 	GC_init();
 	Bytecode* main = Bytecode_loadFromFile(argv[argc - 1]);
-	Stack* stack = Stack_create();
-	long lineNumber = 0;
-	int exitStatus = -1;
-	if ((exitStatus = runBytecode(main, stack, &lineNumber)) != 0) {
-		fprintf(stderr, "Error %d at line %d:\n", exitStatus, lineNumber);
-		dumpStack(stack, stderr);
-		return 1;
-	}
-	return 0;
-}
-void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, long* lineNumber);
-int runBytecode(Bytecode* main, Stack* stack, long* lineNumber) {
 	if (debug) {
 		printf("Class: %s length: %d\n", main->name, main->length);
 		for (int i = 0 ; i < main->length ; i ++) {
-			printf("%c", main->data[i]);
+			short c = main->data[i];
+			if ((c >> 7) & 1) { printf("%4d: String(\"%s\")\n", i, (char*) (&main->data[i] + 1)); i += (main->data[i] ^ (1 << 7)); }
+			else if ((c >> 8) & 1) { printf("%4d: Call(%s)\n", i, (char*) (&main->data[i] + 1)); i += (main->data[i] ^ (1 << 8)); }
+			else if ((c >> 9) & 1) { printf("%4d: Get(%s)\n", i, (char*) (&main->data[i] + 1)); i += (main->data[i] ^ (1 << 9)); }
+			else if ((c >> 10) & 1) { printf("%4d: Set(%s)\n", i, (char*) (&main->data[i] + 1)); i += (main->data[i] ^ (1 << 10)); }
+			else if ((c >> 11) & 1) { printf("%4d: Exit()\n", i); i += (main->data[i] ^ (1 << 11)); }
+			else printf("%4d: %c\n", i, (char) c);
+			//printf("%c", main->data[i]);
 		}
 		printf("\n#####\n");
 	}
+	if (!norun) {
+		Stack* stack = Stack_create();
+		Stack* localIndex = Stack_create();
+		Stack* locals = Stack_create();
+		Stack* localFrame = Stack_create();
+		long lineNumber = 0;
+		int exitStatus = -1;
+		if ((exitStatus = runBytecode(main, stack, localIndex, locals, localFrame, &lineNumber)) != 0) {
+			if (error) {
+				fprintf(stderr, "Error %s at line %d:\n", errorMessage, lineNumber);
+			} else {
+				fprintf(stderr, "Error %d at line %d:\n", exitStatus, lineNumber);
+			}
+			dumpLocals(localIndex, locals, localFrame, stderr);
+			dumpStack(stack, stderr);
+			return 1;
+		}
+	}
+	return 0;
+}
+void exec(Bytecode* main, Stack* stack, Stack* localIndex, Stack* locals, Stack* localFrame, int* running, long* currentLocation, long* lineNumber);
+int runBytecode(Bytecode* main, Stack* stack, Stack* localIndex, Stack* locals, Stack* localFrame, long* lineNumber) {
 	int running = 1;
 	long currentLocation = 0;
 	while (running) {
-		exec(main, stack, &running, &currentLocation, lineNumber);
+		exec(main, stack, localIndex, locals, localFrame, &running, &currentLocation, lineNumber);
+		if (error) {
+			return 1;
+		}
 		do {
 			currentLocation ++;
 			if (currentLocation > main->length) {
@@ -67,7 +95,14 @@ int runBytecode(Bytecode* main, Stack* stack, long* lineNumber) {
 	}
 	return 0;
 }
-void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, long* lineNumber) {
+// Interrupts
+void int_printf(argStack);
+StackItem* int_add(StackItem* lOperand, StackItem* rOperand);
+StackItem* int_subtract(StackItem* lOperand, StackItem* rOperand);
+StackItem* int_multiply(StackItem* lOperand, StackItem* rOperand);
+StackItem* int_divide(StackItem* lOperand, StackItem* rOperand);
+
+void exec(Bytecode* main, Stack* stack, Stack* localIndex, Stack* locals, Stack* localFrame, int* running, long* currentLocation, long* lineNumber) {
 	if (debug > 1) {
 		printf("command: '%c'\n", main->data[(*currentLocation)]);
 	}
@@ -78,8 +113,8 @@ void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, lon
 		long end = -1;
 		int decimal = 0;
 		for (long i = start ; i < main->length ; i ++) {
-			if (main->data[i] < '0' || main->data[i] > '9') {
-				if (main->data[i] == '.') {
+			if ((char) main->data[i] < '0' || main->data[i] > '9') {
+				if ((char) main->data[i] == '.') {
 					decimal = 1;
 				} else {
 					end = i;
@@ -88,7 +123,7 @@ void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, lon
 			}
 		}
 		if (end == -1) {
-			fprintf(stderr, "A function name was not specified at: -\n");
+			fprintf(stderr, "An invalid number was found at: -\n");
 			return 1;
 		}
 		(*currentLocation) = end - 1;
@@ -112,35 +147,24 @@ void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, lon
 	} else if (main->data[(*currentLocation)] == '+') {
 		StackItem* rOperand = Stack_pop(stack);
 		StackItem* lOperand = Stack_pop(stack);
-		Type oType = rOperand->type > lOperand->type ? rOperand->type : lOperand->type;
-		if (oType == Type_STRING) {
-			fprintf(stderr, "strings types can't be used in math!!!\n");
-			exit(1);
-		}
-		if (oType == Type_CHAR) {
-			char* val = GC_MALLOC(sizeof(char));
-			*val = (char) *rOperand->item + (char) *lOperand->item;
-			Stack_push(stack, newStackItem(oType, val));
-		} else if (oType == Type_INT) {
-			int* val = GC_MALLOC(sizeof(int));
-			*val = (int) *rOperand->item + (int) *lOperand->item;
-			Stack_push(stack, newStackItem(oType, val));
-		} else if (oType == Type_FLOAT) {
-			float* val = GC_MALLOC(sizeof(float));
-			fprintf(stderr, "casting int to float is not yet supported on this system!!!\n");
-			exit(1);
-			/*
-			*val = 
-				rOperand->type == Type_INT || rOperand->type == Type_CHAR ? (float) *((int*)rOperand->item) : (float) *((float*)rOperand->item)
-				+ lOperand->type == Type_INT || lOperand->type == Type_CHAR ? (float) *((int*)lOperand->item) : (float) *((float*)lOperand->item);
-			*/
-			Stack_push(stack, newStackItem(oType, val));
-		}
+		Stack_push(stack, int_add(lOperand, rOperand));
+	} else if (main->data[(*currentLocation)] == '-') {
+		StackItem* rOperand = Stack_pop(stack);
+		StackItem* lOperand = Stack_pop(stack);
+		Stack_push(stack, int_subtract(lOperand, rOperand));
+	} else if (main->data[(*currentLocation)] == '*') {
+		StackItem* rOperand = Stack_pop(stack);
+		StackItem* lOperand = Stack_pop(stack);
+		Stack_push(stack, int_multiply(lOperand, rOperand));
+	} else if (main->data[(*currentLocation)] == '/') {
+		StackItem* rOperand = Stack_pop(stack);
+		StackItem* lOperand = Stack_pop(stack);
+		Stack_push(stack, int_divide(lOperand, rOperand));
 	} else if (main->data[(*currentLocation)] == '(') {
 		long label = stack->length;
 		(*currentLocation) ++;
 		while (main->data[(*currentLocation)] != ')') {
-			exec(main, stack, running, currentLocation, lineNumber);
+			exec(main, stack, localIndex, locals, localFrame, running, currentLocation, lineNumber);
 			(*currentLocation) ++;
 		}
 		while (stack->length > label + 1) {
@@ -148,31 +172,54 @@ void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, lon
 			GC_free(item->item);
 			GC_free(item);
 		}
-	} else if (main->data[(*currentLocation)] == '!') {
-		long start = (*currentLocation) + 1;
-		//printf("found function call at %d\n", currentLocation);
-		long end = -1;
-		for (long i = start ; i < main->length ; i ++) {
-			if (main->data[i] == '(') {
-				end = i;
+	} else if ((main->data[(*currentLocation)] >> 9) & 1) {
+// Get Variable
+		char* varName = (char*) &main->data[(*currentLocation) + 1];
+		int length = (main->data[(*currentLocation)] ^ (1 << 9));
+		(*currentLocation) += length;
+		int found = 0;
+		for (int i = 0 ; i < locals->length ; i ++) {
+			LocalIndex* local = (LocalIndex*) ((StackItem*) locals->data[i])->item;
+			if (strcmp(local->name, varName) == 0) {
+				found = 1;
+				Stack_push(stack, local->value);
 				break;
 			}
 		}
-		if (end == -1) {
-			fprintf(stderr, "A function name was not specified at: -\n");
-			return 1;
+		if (!found) {
+			setError("variable used before assignment");
+			running = 0;
 		}
-		const long length = end - start;
-		char* funcName = GC_MALLOC(sizeof(char) * (length + 1));
-		for (long i = 0 ; i < length ; i ++) {
-			funcName[i] = main->data[start + i];
+	} else if ((main->data[(*currentLocation)] >> 10) & 1) {
+// Set Variable
+		char* varName = (char*) &main->data[(*currentLocation) + 1];
+		int length = (main->data[(*currentLocation)] ^ (1 << 10));
+		(*currentLocation) += length;
+		int found = 0;
+		for (int i = 0 ; i < locals->length ; i ++) {
+			LocalIndex* local = (LocalIndex*) ((StackItem*) locals->data[i])->item;
+			if (strcmp(local->name, varName) == 0) {
+				found = 1;
+				local->value = Stack_pop(stack);
+				break;
+			}
 		}
-		funcName[length] = 0;
+		if (!found) {
+			LocalIndex* local = GC_MALLOC(sizeof(LocalIndex));
+			local->name = varName;
+			local->value = Stack_pop(stack);
+			Stack_push(locals, newStackItem(Type_LOCAL_INDEX, local));
+			if (debug) printf("var assigned: %s\n", StackItem_toString(locals->data[locals->length-1]));
+		}
+	} else if ((main->data[(*currentLocation)] >> 8) & 1) {
+// Execute Function
+		char* funcName = (char*) &main->data[(*currentLocation) + 1];
+		int length = (main->data[(*currentLocation)] ^ (1 << 8));
+		(*currentLocation) += length;
 		//printf("function name is: %s\n", funcName);
 		long label = stack->length;
-		(*currentLocation) = end + 1;
 		while (main->data[(*currentLocation)] != ')') {
-			exec(main, stack, running, currentLocation, lineNumber);
+			exec(main, stack, localIndex, locals, localFrame, running, currentLocation, lineNumber);
 			(*currentLocation) ++;
 		}
 		if (debug) {
@@ -183,38 +230,14 @@ void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, lon
 			while (stack->length > label) {
 				Stack_push(argStack, Stack_pop(stack));
 			}
-			StackItem* format = Stack_pop(argStack);
-			if (format->type == Type_STRING) {
-				int start1 = 0, end1 = -1;
-				char* fData = ((char*)format->item);
-				int fLen = strlen(fData);
-				int arg = 0;
-				for (int i = 0 ; i <= fLen ; i ++) {
-					if (fData[i] == '%' || i == fLen) {
-						end1 = i;
-						int len = end1 - start1;
-						char* temp = GC_MALLOC(sizeof(char) * (len + 1));
-						for (int i = 0 ; i < len ; i ++) {
-							temp[i] = fData[start1 + i];
-						}
-						//snprintf(fData + start1, len, "%s", temp);
-						temp[len] = 0;
-						if (arg) {
-							StackItem* item = Stack_pop(argStack);
-							printf(temp, 
-								item->type == Type_CHAR ? *((char*)item->item) :
-								item->type == Type_INT ? *((int*)item->item) :
-								//item->type == Type_FLOAT ? *((float*)item->item) :
-								item->type == Type_STRING ? ((char*)item->item) : NULL
-							);
-						} else {
-							printf(temp);
-							arg = 1;
-						}
-						GC_free(temp);
-						start1 = i;
-					}
-				}
+			int_printf(argStack);
+			GC_free(argStack);
+		} else if (strcmp(funcName, "dumpLocals") == 0) {
+			dumpLocals(localIndex, locals, localFrame, stdout);
+			while (stack->length > label) {
+				StackItem* item = Stack_pop(stack);
+				GC_free(item->item);
+				GC_free(item);
 			}
 		} else if (strcmp(funcName, "dumpStack") == 0) {
 			dumpStack(stack, stdout);
@@ -224,45 +247,180 @@ void exec(Bytecode* main, Stack* stack, int* running, long* currentLocation, lon
 				GC_free(item);
 			}
 		}
-		//printf("next command will be: '%c'\n", main->data[(*currentLocation)]);
-	} else if (main->data[(*currentLocation)] == '"') {
-		long start = (*currentLocation) + 1;
-		//printf("found string at %d\n", currentLocation);
-		long end = -1;
-		for (long i = start ; i < main->length ; i ++) {
-			if (main->data[i] == '"') {
-				end = i;
-				break;
-			}
+	} else if ((main->data[(*currentLocation)] >> 7) & 1) {
+// string constant
+		char* val = (char*) &main->data[(*currentLocation) + 1];
+		int length = (main->data[(*currentLocation)] ^ (1 << 7));
+		/*
+		for (int i = 0 ; i < length ; i ++) {
+			printf("char: %d at: %d\n", val[i], i);
 		}
-		if (end == -1) {
-			fprintf(stderr, "A function name was not specified at: -\n");
-			return 1;
-		}
-		const long length = end - start;
-		char* stringData = GC_MALLOC(sizeof(char) * (length - 1));
-		int escaped = 0, offset = 0;
-		for (long i = 0 ; i < length; i ++) {
-			char c = main->data[start + i];
-			if (c == '\\') {
-				escaped = 1;
-				offset ++;
-			} else if (escaped) {
-				escaped = 0;
-				if (c == 'n') {
-					stringData[i - offset] = '\n';
-				} else if (c == 't') {
-				} else {
-					stringData[i - offset] = '\t';
-				}
-			} else {
-				stringData[i - offset] = c;
-			}
-		}
-		stringData[length] = 0;
-		Stack_push(stack, newStackItem(Type_STRING, stringData));
-		(*currentLocation) = end;
+		*/
+		//printf("string: %s length: %d\n", val, length);
+		(*currentLocation) += length;
+		Stack_push(stack, newStackItem(Type_STRING, val));
+	} else if ((main->data[(*currentLocation)] >> 11) & 255) {
+		exit(0);
 	}
+}
+// Interrupt Implementation
+StackItem* int_add(StackItem* lOperand, StackItem* rOperand) {
+	Type oType = rOperand->type > lOperand->type ? rOperand->type : lOperand->type;
+	if (oType == Type_STRING) {
+		fprintf(stderr, "strings types can't be used in math!!!\n");
+		exit(1);
+	}
+	if (oType == Type_CHAR) {
+		char* val = GC_MALLOC(sizeof(char));
+		*val = (char) *lOperand->item + (char) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_INT) {
+		int* val = GC_MALLOC(sizeof(int));
+		*val = (int) *lOperand->item + (int) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_FLOAT) {
+		float* val = GC_MALLOC(sizeof(float));
+		fprintf(stderr, "casting int to float is not yet supported on this system!!!\n");
+		exit(1);
+		/*
+		*val = 
+			lOperand->type == Type_INT || lOperand->type == Type_CHAR ? (float) *((int*)lOperand->item) : (float) *((float*)lOperand->item)
+			+ rOperand->type == Type_INT || rOperand->type == Type_CHAR ? (float) *((int*)rOperand->item) : (float) *((float*)rOperand->item);
+		*/
+		return newStackItem(oType, val);
+	}
+}
+StackItem* int_subtract(StackItem* lOperand, StackItem* rOperand) {
+	Type oType = rOperand->type > lOperand->type ? rOperand->type : lOperand->type;
+	if (oType == Type_STRING) {
+		fprintf(stderr, "strings types can't be used in math!!!\n");
+		exit(1);
+	}
+	if (oType == Type_CHAR) {
+		char* val = GC_MALLOC(sizeof(char));
+		*val = (char) *lOperand->item - (char) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_INT) {
+		int* val = GC_MALLOC(sizeof(int));
+		*val = (int) *lOperand->item - (int) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_FLOAT) {
+		float* val = GC_MALLOC(sizeof(float));
+		fprintf(stderr, "casting int to float is not yet supported on this system!!!\n");
+		exit(1);
+		/*
+		*val = 
+			lOperand->type == Type_INT || lOperand->type == Type_CHAR ? (float) *((int*)lOperand->item) : (float) *((float*)lOperand->item)
+			- rOperand->type == Type_INT || rOperand->type == Type_CHAR ? (float) *((int*)rOperand->item) : (float) *((float*)rOperand->item);
+		*/
+		return newStackItem(oType, val);
+	}
+}
+StackItem* int_multiply(StackItem* lOperand, StackItem* rOperand) {
+	Type oType = rOperand->type > lOperand->type ? rOperand->type : lOperand->type;
+	if (oType == Type_STRING) {
+		fprintf(stderr, "strings types can't be used in math!!!\n");
+		exit(1);
+	}
+	if (oType == Type_CHAR) {
+		char* val = GC_MALLOC(sizeof(char));
+		*val = (char) *lOperand->item * (char) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_INT) {
+		int* val = GC_MALLOC(sizeof(int));
+		*val = (int) *lOperand->item * (int) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_FLOAT) {
+		float* val = GC_MALLOC(sizeof(float));
+		fprintf(stderr, "casting int to float is not yet supported on this system!!!\n");
+		exit(1);
+		/*
+		*val = 
+			lOperand->type == Type_INT || lOperand->type == Type_CHAR ? (float) *((int*)lOperand->item) : (float) *((float*)lOperand->item)
+			* rOperand->type == Type_INT || rOperand->type == Type_CHAR ? (float) *((int*)rOperand->item) : (float) *((float*)rOperand->item);
+		*/
+		return newStackItem(oType, val);
+	}
+}
+StackItem* int_divide(StackItem* lOperand, StackItem* rOperand) {
+	Type oType = rOperand->type > lOperand->type ? rOperand->type : lOperand->type;
+	if (oType == Type_STRING) {
+		fprintf(stderr, "strings types can't be used in math!!!\n");
+		exit(1);
+	}
+	if (oType == Type_CHAR) {
+		char* val = GC_MALLOC(sizeof(char));
+		*val = (char) *lOperand->item / (char) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_INT) {
+		int* val = GC_MALLOC(sizeof(int));
+		*val = (int) *lOperand->item / (int) *rOperand->item;
+		return newStackItem(oType, val);
+	} else if (oType == Type_FLOAT) {
+		float* val = GC_MALLOC(sizeof(float));
+		fprintf(stderr, "casting int to float is not yet supported on this system!!!\n");
+		exit(1);
+		/*
+		*val = 
+			lOperand->type == Type_INT || lOperand->type == Type_CHAR ? (float) *((int*)lOperand->item) : (float) *((float*)lOperand->item)
+			/ rOperand->type == Type_INT || rOperand->type == Type_CHAR ? (float) *((int*)rOperand->item) : (float) *((float*)rOperand->item);
+		*/
+		return newStackItem(oType, val);
+	}
+}
+void int_printf(Stack* argStack) {
+	StackItem* format = Stack_pop(argStack);
+	if (format->type == Type_STRING) {
+		int start = 0, end = -1;
+		char* fData = ((char*)format->item);
+		int fLen = strlen(fData);
+		int arg = 0;
+		for (int i = 0 ; i <= fLen ; i ++) {
+			if (fData[i] == '%' || i == fLen) {
+				end = i;
+				int len = end - start;
+				char* temp = GC_MALLOC(sizeof(char) * (len + 1));
+				for (int i = 0 ; i < len ; i ++) {
+					temp[i] = fData[start + i];
+				}
+				//snprintf(fData + start1, len, "%s", temp);
+				temp[len] = 0;
+				if (arg) {
+					StackItem* item = Stack_pop(argStack);
+					printf(temp, 
+						item->type == Type_CHAR ? *((char*)item->item) :
+						item->type == Type_INT ? *((int*)item->item) :
+						//item->type == Type_FLOAT ? *((float*)item->item) :
+						item->type == Type_STRING ? ((char*)item->item) : NULL
+					);
+				} else {
+					printf(temp);
+					arg = 1;
+				}
+				GC_free(temp);
+				start = i;
+			}
+		}
+	}
+}
+void setError(char* message) {
+	if (debug) {
+		printf("setting error message\n");
+	}
+	error = 1;
+	errorMessage = GC_MALLOC(sizeof(char) * strlen(message));
+	strcpy(errorMessage, message);
+}
+void dumpLocals(Stack* localIndex, Stack* locals, Stack* localFrame, FILE* stream) {
+	fprintf(stream, "Locals:[");
+	for (int i = 0 ; i < locals->length ; i ++) {
+		StackItem* item = locals->data[i];
+		fprintf(stream, "%s", StackItem_toString(item));
+		if (i < locals->length - 1) {
+			fprintf(stream, ", ");
+		}
+	}
+	fprintf(stream, "]\n");
 }
 void dumpStack(Stack* stack, FILE* stream) {
 	fprintf(stream, "Stack:[");
